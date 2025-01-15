563f6f57b7eb3d57f14d5196913bc6c0fb74f3f9
satyam kr <satyam.9242@gmail.com>
2025-01-16 05:03:15 +0530

    user updating part done

--- server/controllers/users.ts
+++ server/controllers/users.ts
@@ -68,11 +68,60 @@ async function createUser(req: Request, res: Response) {
             // this will insert all the hobbies concurrently
             await Promise.all(hobbyQueries);
         }
-        
+
         res.status(201).json({ message: "User created successfully" });
     } catch (error) {
         console.error("Error fetching users:", error);
         res.status(500).json({ error: "Internal Server Error" });
     }
 }
-export { getUsers, createUser };
+
+// update user handler
+async function updateUser(req: Request, res: Response){
+    try{
+        const id: number =  parseInt(req.params.id);
+        const result = await client.query(`SELECT * FROM USERS WHERE id = ($1)`, [id]);
+
+        if(result.rows.length === 0){
+            res.status(404).json({message: "User not exist"});
+            return;
+        }
+
+        const user: Users = req.body;
+
+        if (user.name.length === 0 || user.age <= 0) {
+            res.status(400).json({ message: "Required valid Name or Age" });
+            return;
+        }
+
+        // updating name and age
+        if(user.name != result.rows[0].name && user.age != result.rows[0].age){
+            await client.query(`UPDATE users SET name = ($1), age = ($2) WHERE id = ($3)`, [user.name, user.age, id]);
+        }else if(user.name != result.rows[0].name){
+            await client.query(`UPDATE users SET name = ($1) WHERE id = ($2)`, [user.name, id]);
+        }else if(user.age != result.rows[0].age){
+            await client.query(`UPDATE users SET age = ($1) WHERE id = ($2)`, [user.age, id]);
+        }
+
+        // deleting all the hobbies related to that user
+        await client.query(`DELETE FROM hobbies WHERE id = ($1)`, [id]);
+
+        if (user.hobbies && user.hobbies.length > 0) {
+            const hobbyQueries = user.hobbies.map((hobby) =>
+                client.query(
+                    `INSERT INTO hobbies (id, hobbies) VALUES ($1, $2)`,
+                    [result.rows[0].id, hobby]
+                )
+            );
+
+            // this will insert all the hobbies concurrently
+            await Promise.all(hobbyQueries);
+        }
+
+        res.status(200).json({ message: "User updated successfully" });
+    }catch(error){
+        console.error("Error fetching users:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+export { getUsers, createUser, updateUser };
