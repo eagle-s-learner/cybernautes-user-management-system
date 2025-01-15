0223ffa29790bc60f6dc4f74db21d839a1037803
satyam kr <satyam.9242@gmail.com>
2025-01-16 04:28:40 +0530

    add new user routes completed

--- server/controllers/users.ts
+++ server/controllers/users.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import client from "../config";
 
 interface Users {
-    id: number;
+    id?: number;
     name: string;
     age: number;
     hobbies?: string[];
@@ -11,8 +11,6 @@ interface Users {
 // to handle get all the users and there hobbies
 async function getUsers(req: Request, res: Response) {
     try {
-        const { name } = req.body;
-        console.log(name);
         const query = `
             SELECT 
                 u.id, 
@@ -21,7 +19,8 @@ async function getUsers(req: Request, res: Response) {
                 ARRAY_AGG(h.hobbies) AS hobbies
             FROM users u
             LEFT JOIN hobbies h ON u.id = h.id
-            GROUP BY u.id, u.name, u.age;
+            GROUP BY u.id, u.name, u.age
+            ORDER BY u.id DESC;
         `;
 
         const result = await client.query(query);
@@ -41,4 +40,39 @@ async function getUsers(req: Request, res: Response) {
     }
 }
 
-export { getUsers };
+// create new user
+async function createUser(req: Request, res: Response) {
+    try {
+        const user: Users = req.body;
+
+        if (user.name.length === 0 || user.age <= 0) {
+            res.status(400).json({ message: "Required valid Name or Age" });
+            return;
+        }
+
+        // this query will insert and return the inserted user at the same time
+        const result = await client.query(
+            `INSERT INTO users (name, age) VALUES ($1, $2) RETURNING *`,
+            [user.name, user.age]
+        );
+
+        // now insert the hobbies in hobbies table
+        if (user.hobbies && user.hobbies.length > 0) {
+            const hobbyQueries = user.hobbies.map((hobby) =>
+                client.query(
+                    `INSERT INTO hobbies (id, hobbies) VALUES ($1, $2)`,
+                    [result.rows[0].id, hobby]
+                )
+            );
+
+            // this will insert all the hobbies concurrently
+            await Promise.all(hobbyQueries);
+        }
+        
+        res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+export { getUsers, createUser };
