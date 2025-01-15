8585369247a630d0f23d5aea1d11079891b8083a
satyam kr <satyam.9242@gmail.com>
2025-01-16 04:33:04 +0530

    added update user routes

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/users";
+import { createUser, getUsers, updateUser } from "../controllers/users";
 
 const routes = Router();
 
 routes.get('/users/', getUsers);
 
 routes.post('/users', createUser);
+
+routes.put('/users/:id', updateUser);
 
 export default routes;
