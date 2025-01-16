024cd24fb1fa07961fc7a13acad18655f4840f56
satyam kr <satyam.9242@gmail.com>
2025-01-16 09:47:52 +0530

    layout for main page done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
+import MainArea from "./components/MainArea"
+import SideBar from "./components/SideBar"
+
 function App() {
 
   return (
     <>
-      <h1 className="p-16">React set up</h1>
+      <div className="sticky top-0 mx-auto bg-slate-400">
+        <h1 className="text-2xl w-fit mx-auto font-semibold p-2">User Mangement System</h1>
+        <button className="absolute top-1 p-2 hover:underline right-2">Create User</button>
+      </div>
+      <div className="flex">
+        <SideBar />
+        <MainArea />
+      </div>
     </>
   )
 }
