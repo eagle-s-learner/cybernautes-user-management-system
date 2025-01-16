6c30b58ab5998e777497c0c5a7fd98168458ca8b
satyam kr <satyam.9242@gmail.com>
2025-01-16 19:42:59 +0530

    added cors policy

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,27 @@
 import express from "express";
-import dotenv  from "dotenv";
+import dotenv from "dotenv";
 dotenv.config();
 import bodyParser from "body-parser";
 import usersRoutes from "./routes/users";
+import cors from "cors";
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:5173",
+    })
+);
 
-app.use('/api', usersRoutes);
-
+app.use("/api", usersRoutes);
 
 const port = process.env.PORT || 6001;
 
 app.listen(port, (err) => {
-    if(err){
-        console.log(err)
+    if (err) {
+        console.log(err);
     }
     console.log(`Server is running on port: ${port}`);
-})
+});
