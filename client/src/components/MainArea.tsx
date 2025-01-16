25e725c090d1ac68e690cfd9704274af03178ab1
satyam kr <satyam.9242@gmail.com>
2025-01-16 09:26:48 +0530

    SideBar layout and Main Area Layout

---
+++ client/src/components/MainArea.tsx
@@ -0,0 +1,7 @@
+export default function MainArea(){
+    return (
+        <div className="h-screen w-full">
+            <h1>main area</h1>
+        </div>
+    )
+}
---
+++ client/src/components/SideBar.tsx
@@ -0,0 +1,7 @@
+export default function SideBar(){
+    return (
+        <div className="w-64 bg-gray-100 p-4">
+            <h1>hello</h1>
+        </div>
+    )
+}
